949ac9fd3d367f2b15eefc8f29f2b6414a3b6b9d
vlopmun831 <vanesaopos81@gmail.com>
2024-11-25 08:48:04 +0100

    actividas

--- ActividadesTema2/src/actividadesboletin/Actividad3.java
+++ ActividadesTema2/src/actividadesboletin/Actividad3.java
@@ -10,10 +10,32 @@ public class Actividad3 {
 		Scanner scan = new Scanner (System.in);
 		
 		System.out.println("Dime una letra en mayúscula o minúscula");
+		int numero;
 
 		String letra = scan.next();
 		
-		if ()
+		// a == A
+		// A == A
+		
+		if (letra.equals(letra.toUpperCase())) {
+			System.out.println("Es una mayúscula");
+		}else if(letra.equals(letra.toLowerCase()))  {
+			System.out.println("Es minúscula");
+		}
+
+		//Es mayuscula
+		if(letra.matches("[A-Z]")) {
+			
+		}
+		// Es minuscula
+		else if(letra.matches("[a-z]")) {
+			
+		}
+		//No es una letra
+		else {
+			
+		}
+			
 	}
 
 }
